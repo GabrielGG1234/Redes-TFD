b894bf44b8feb431f78c43c6f9f130c2e0604dfc
Gabriel Antunes <xablauia74@gmail.com>
2022-12-06 03:35:20 -0300

    Impossível conectar o ansible =/

--- provisions/docker-notas.sh
+++ provisions/docker-notas.sh
@@ -1,2 +1,14 @@
-docker build -t devops/notes-docker
-docker run --network devops --hostname app -p 8080:8080 -d devops/notes-docker
+
+sudo apt install nano
+sudo nano Dockerfile
+
+FROM openjdk:8-jdk-alpine
+RUN addgroup -S notes && adduser -S notes -G notes
+USER notes:notes
+ARG JAR_FILE=*.jar
+COPY ${JAR_FILE} easy-note.jar
+COPY application.properties application.properties
+ENTRYPOINT ["java","-jar","/easy-note.jar"]
+
+sudo docker build -t devops/notes-docker
+sudo docker run --network devops --hostname app -p 8080:8080 -d devops/notes-docker
--- provisions/workers.sh
+++ provisions/workers.sh
@@ -1,6 +1,6 @@
 
 echo "copie o comando a seguir e rode nas vms 1 e 2"
-sudo docker swarm join-token manager
+
 sudo reboot
 
 # Colar comando substituir e executar nas vms secundárias
